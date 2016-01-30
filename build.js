134c7268fc7dd6de8e975d3b916ad149c985aca7
Matt McKegg <matt@wetsand.co.nz>
2016-01-30 20:38:40 +1300

    update for latest version

--- build.js
+++ build.js
@@ -7,9 +7,10 @@ var appdmg = require('appdmg')
 var packageMsi = require('msi-packager')
 var path = require('path')
 var each = require('each-async')
+var sign = require('electron-osx-sign')
 
 var buildFrom = '/Users/matt/Code/loop-drop-app'
-var appPackage = require(buildFrom  + '/package.json')
+var appPackage = require(buildFrom + '/package.json')
 
 var platform = process.argv[2]
 var arch = process.argv[3]
@@ -22,29 +23,29 @@ process.cwd(__dirname)
 rimraf.sync('build')
 
 electronPackager({
- dir: buildFrom,
- out: 'build',
- name: 'Loop Drop',
- arch: arch,
- overwrite: true,
- platform: platform,
- version: '0.33.7',
- appVersion: appPackage.version,
- prune: true,
- asar: true,
- icon: 'icon.ext',
- ignore: [
-   '/demo-project',
-   'test/',
-   'tests/',
-   'sounds/',
-   'doc/',
-   'mocha/',
-   'node_modules/midi/',
-   'testling/',
-   'web-fs/',
-   'browserify-fs/'
- ],
+  dir: buildFrom,
+  out: 'build',
+  name: 'Loop Drop',
+  arch: arch,
+  overwrite: true,
+  platform: platform,
+  version: '0.36.6',
+  appVersion: appPackage.version,
+  prune: true,
+  asar: true,
+  icon: 'icon.ext',
+  ignore: [
+    '/demo-project',
+    'test/',
+    'tests/',
+    'sounds/',
+    'doc/',
+    'mocha/',
+    'node_modules/midi/',
+    'testling/',
+    'web-fs/',
+    'browserify-fs/'
+  ]
 }, copyDemoProject)
 
 function copyDemoProject (err) {
@@ -77,7 +78,6 @@ function copyDemoProject (err) {
 }
 
 function packageRelease () {
-
   if (platform === 'darwin' || platform === 'all') {
     packageForMac()
   }
@@ -93,62 +93,44 @@ function packageRelease () {
   }
 }
 
-function signAppBundle (identity, files) {
-  files.forEach(function(file) {
-    execFileSync('codesign', [
-      '-s', identity,
-      '-f', file
-    ])
-  })
-}
-
 function packageForMac () {
-  var outputPath = "releases/Loop Drop v" + appPackage.version + '.dmg'
+  var outputPath = 'releases/Loop Drop v' + appPackage.version + '.dmg'
   rimraf.sync(outputPath)
 
   console.log('Signing app bundle')
 
   var bundle = 'build/Loop Drop-darwin-x64/Loop Drop.app'
-  var frameworks = bundle + '/Contents/Frameworks'
-
-  signAppBundle('989QCZP56K', [
-    frameworks + "/Loop Drop Helper.app",
-    frameworks + "/Mantle.framework",
-    frameworks + "/Electron Framework.framework/Versions/Current/Electron Framework",
-    frameworks + "/Electron Framework.framework",
-    frameworks + "/Electron Helper NP.app",
-    frameworks + "/ReactiveCocoa.framework",
-    frameworks + "/Electron Helper EH.app",
-    frameworks + "/Squirrel.framework",
-    bundle
-  ])
-
-  console.log('Creating dmg')
-  appdmg({
-    basepath: __dirname,
-    target: outputPath,
-    specification: {
-      'title': "Loop Drop",
-      'icon': "icon.icns",
-      'background': "dmg.png",
-      'icon-size': 100,
-      'contents': [
-        { x: 430, y: 190, type: "link", path: "/Applications" },
-        { x: 170, y: 190, type: "file", path: bundle }
-      ]
-    }
-  }).on('finish', function () {
-    console.log('Output to ' + path.resolve(outputPath))
-  }).on('error', function (err) {
-    throw err
+  sign({
+    app: bundle
+  }, function (err) {
+    if (err) throw err
+    console.log('Creating dmg')
+    appdmg({
+      basepath: __dirname,
+      target: outputPath,
+      specification: {
+        'title': 'Loop Drop',
+        'icon': 'icon.icns',
+        'background': 'dmg.png',
+        'icon-size': 100,
+        'contents': [
+          { x: 430, y: 190, type: 'link', path: '/Applications' },
+          { x: 170, y: 190, type: 'file', path: bundle }
+        ]
+      }
+    }).on('finish', function () {
+      console.log('Output to ' + path.resolve(outputPath))
+    }).on('error', function (err) {
+      throw err
+    })
   })
 }
 
 function packageForWindows (arch) {
   console.log('Creating msi')
-  var outputPath = arch === 'x64' ?
-    'releases/Loop Drop v' + appPackage.version + ' x64.msi' :
-    'releases/Loop Drop v' + appPackage.version + '.msi'
+  var outputPath = arch === 'x64'
+    ? 'releases/Loop Drop v' + appPackage.version + ' x64.msi'
+    : 'releases/Loop Drop v' + appPackage.version + '.msi'
 
   packageMsi({
     source: 'build/Loop Drop-win32-' + arch,
